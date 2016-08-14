a6d3b3dbc2d5fa62f69548baca0702b5e931e0b3
Takuaki Mori <t.mori@freebit.net>
2016-08-14 12:00:39 +0900

    use place() method insteads of pack() method.

--- app/view/entry_viewer.py
+++ app/view/entry_viewer.py
@@ -7,10 +7,10 @@ Exit   ゲーム終了
 Score  スコア表の表示
 '''
 
-
 class EntryViewer(tk.Canvas):
     Width = 800  # 画面横幅
     Height = 600  # 画面縦幅
+    SideMargin = 100
 
     def __init__(self, master, controller=None):
         super().__init__(master, bg="white", width=self.Width, height=self.Height)
@@ -18,16 +18,26 @@ class EntryViewer(tk.Canvas):
         # controller
         self.controller = controller
 
-        # button
-        self.button_start = tk.Button(master, text="Start", command=lambda: None)
-        self.button_exit = tk.Button(master, text="Exit", command=lambda: None)
-        self.button_score = tk.Button(master, text="Stop", command=lambda: None)
+        # title image size=(300,300)
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.img_title = Image.open(os.path.join(script_dir, 'static/title.png'))
+        self.img_title.resize((self.Width-2*self.SideMargin,200))
+        self.tkimg_title = ImageTk.PhotoImage(self.img_title)
+        self.label_tkimg_title = tk.Label(self,image=self.tkimg_title)
+        self.label_tkimg_title.place(relx=0.15,rely=0.1)
 
-        # create view
+
+        # button Frame
+        buttonFrame = tk.Frame(master=self,width=100,height=200)
+        self.button_start = tk.Button(buttonFrame, text="Start",command=lambda: None)
+        self.button_start.pack(side=tk.TOP)
+        self.button_exit = tk.Button(buttonFrame, text="Exit", command=lambda: None)
+        self.button_exit.pack(side=tk.TOP)
+        self.button_score = tk.Button(buttonFrame, text="Stop", command=lambda: None)
+        self.button_score.pack(side=tk.TOP)
+        buttonFrame.place(relx=0.5,rely=0.5)
+
         self.pack()
-        self.button_start.pack(anchor=tk.S, side=tk.BOTTOM)
-        self.button_exit.pack(anchor=tk.S, side=tk.BOTTOM)
-        self.button_score.pack(anchor=tk.S, side=tk.BOTTOM)
 
     def animate(self):
         pass
