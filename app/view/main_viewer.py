f66fca4797d154a3307b9ebc86343ae44740bf65
Takuaki Mori <t.mori@freebit.net>
2016-09-25 12:28:07 +0900

    separate 2 window with grid function

--- app/view/main_viewer.py
+++ app/view/main_viewer.py
@@ -7,7 +7,29 @@ class MainViewer(tk.Canvas):
 
     def __init__(self, master, controller=None):
         super().__init__(master, bg="white", width=self.Width, height=self.Height)
+
+        gw = GameWindow(self)
+        sw = StatusWindow(self)
+
+        gw.grid(column=0, row=0)
+        sw.grid(column=1, row=0)
         self.pack()
-        
+
     def animate(self):
         pass
+
+
+class GameWindow(tk.Frame):
+    Width = 600
+    Height = 600
+
+    def __init__(self, master):
+        super().__init__(master, bg="black", width=self.Width, height=self.Height)
+
+
+class StatusWindow(tk.Frame):
+    Width = 200
+    Height = 600
+
+    def __init__(self, master):
+        super().__init__(master, bg="white", width=self.Width, height=self.Height)
